3f4bf6ced4601eca30f64d4e9de835cc8accd21b
SergeitZ <sergiofranco6@gmail.com>
2022-03-24 18:12:12 -0400

    completed warmup Anrgry Professor

---
+++ src/com/company/warmup/AngryProfessor.java
@@ -0,0 +1,29 @@
+package com.company.warmup;
+
+import java.util.List;
+
+public class AngryProfessor {
+    // Hakerrank warmup Algorithm
+    public static String angryProfessor(int k, List<Integer> a) {
+        // Write your code here
+
+        int onTime = 0;
+
+        for (Integer time : a) {
+            if (time <= 0) {
+                onTime++;
+            }
+        }
+
+        if (onTime >= k) {
+            return "NO";
+        } else {
+            return "YES";
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
